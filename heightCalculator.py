28fa88e0b8232a850b985245c6fbfb73cb6acdfc
Krzysztof Tomaszewski <krzysztofdtomaszewski@gmail.com>
2021-05-15 22:27:17 +0100

    Most Advanced Height Caculator YET

---
+++ heightCalculator.py
@@ -0,0 +1,9 @@
+# NOTES: In Python YOU DON'T:
+# USE SEMI-COLONS AT END OF EACH STATEMENT
+# INTERPRETED FROM ZSH
+
+num1 = input("What is your height? ")
+
+result = float(num1)
+
+print("Your  height: ", result)
