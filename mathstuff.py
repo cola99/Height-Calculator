edca9706843f4ac973194618acdca5d866095ca5
matt <61066167+cola99@users.noreply.github.com>
2021-05-17 14:45:55 +0100

    huj

--- mathstuff.py
+++ mathstuff.py
@@ -1,5 +1,8 @@
 from tkinter import *
+from result import *
 
+var1 = {}
+var2 = "cm"
 
 def MathStuff():
 
--- result.py
+++ result.py
@@ -1,11 +1,11 @@
 from tkinter import *
-
+from mathstuff import *
 
 def Result():
  
     # Sub Window
     sub = Toplevel()
     sub.title("Result")
-    sub_text = Label(sub, text = 'Your Height is: ' )
+    sub_text = Label(sub, text = 'Your Height is:')
     sub_text.pack()
     sub.geometry("300x100")
