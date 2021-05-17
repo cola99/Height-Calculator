ffe7302fb0574122d2c007bee9e3d926f62ca80f
matt <61066167+cola99@users.noreply.github.com>
2021-05-17 16:05:02 +0100

    final

--- heightCalculator.py
+++ heightCalculator.py
@@ -3,7 +3,7 @@ from tkinter import * # Tk
  # Window
 root = Tk()
 root.title('Height Calculator')
-root.geometry("400x200")
+root.geometry("300x100")
 root.resizable(False, False)
  
 def returnEntry():
@@ -11,21 +11,20 @@ def returnEntry():
     resultLabel.config(text="Your Height is roughly: " + result + "cm")
  
 # Label
-topLabl = Label(root,text="Enter Your Height: ")
-topLabl.grid(column=0, row=0, padx=20, pady=30)
+topLabl = Label(root,text="Enter Your Height: (cm) ")
+topLabl.pack()
 
 # Entry
 myEntry = Entry(root, width=20)
-myEntry.bind("<Return>",returnEntry)
-myEntry.grid(column=1, row=0, padx=20, pady=30)
+myEntry.pack()
  
 # Button
 enterEntry = Button(root, text= "Calculate", command=returnEntry)
-enterEntry.grid(column=2, row=0, padx=20, pady=30)
+enterEntry.pack(pady=5)
  
 # Emplty Label
 resultLabel = Label(root, text = "")
-resultLabel.grid()
+resultLabel.pack()
 
 
 root.mainloop()
