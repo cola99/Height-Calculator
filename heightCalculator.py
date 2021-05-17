d0f5bbdbcc809e6f5489965317778df153f91fbf
matt <61066167+cola99@users.noreply.github.com>
2021-05-17 01:37:16 +0100

    remove clutter

--- heightCalculator.py
+++ heightCalculator.py
@@ -2,14 +2,6 @@ import tkinter
 
 root = tkinter.Tk()
 root.title("Most Advanced Calculator")
-
-label_result = tkinter.Label(root, text="")
-label_result.grid(row=0, column=0)
-
-button_1 = tkinter.Button(root, text="1", command=add())
-button_1.grid(row=1, column=0)
-
-button_2 = tkinter.Button(root, text="2", command=add())
-button_2.grid(row=1, column=0)
+root.geometry("500x200")
 
 root.mainloop()
