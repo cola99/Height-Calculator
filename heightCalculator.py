0bcbd1fb39314892aea7a345e042f1ee693b4156
matt <61066167+cola99@users.noreply.github.com>
2021-05-17 15:22:01 +0100

    fix

--- heightCalculator.py
+++ heightCalculator.py
@@ -1,25 +1,21 @@
 from tkinter import * # Tk
 
-
 root = Tk()       
 root.title("Most Advanced Calculator")      
 root.geometry('550x100')
 root.resizable(False, False)
 
-# Global Vars
-var1 = StringVar("")
-
 # Labels
 label_1 = Label(root, text="Enter your height:")
-label_1.grid(column=0, row=0, padx = 15, pady = 30)
+label_1.grid(column=0, row=0, padx=15, pady=30)
 
 # Entrys
-entry_1 = Entry(root, width=50, textvariable=var1)
-entry_1.grid(column=1, row=0, padx = 15, pady = 5)
+entry_1 = Entry(root, width=50)
+entry_1.grid(column=1, row=0, padx=15, pady=5)
   
 # Buttons
 button1 = Button(root, text="Calculate")
-button1.grid(column=2, row=0, padx = 15, pady = 5)
+button1.grid(column=2, row=0, padx=15, pady=5)
 
 root.mainloop()
 
