4917b5bb05bb4809bfa843d395bd8594d1809c17
matt <61066167+cola99@users.noreply.github.com>
2021-05-17 13:06:33 +0100

    Fixes

--- heightCalculator.py
+++ heightCalculator.py
@@ -1,11 +1,21 @@
-import tkinter
+
+from tkinter import *   
+
+def MainWindow():
+    root = Tk()       
+    root.title("Most Advanced Calculator")      
+    root.geometry('500x200')
+
+    top_text = Label (root, text = 'Guwno w gaciah')
+
+    top_text.pack(side = 'top', padx = '10', pady = '10')
 
 
-def Window():
-    root = tkinter.Tk()
-    root.title("Most Advanced Calculator")
-    root.geometry("500x200")
+    calculate_button = Button(root, text = 'Calculate', bd = '3', padx = '10', pady = '10', command = root.destroy)
+    
 
+    calculate_button.pack(side = 'bottom', padx = '20', pady = '20')   
+    
     root.mainloop()
-
-Window()
+    
+MainWindow()
